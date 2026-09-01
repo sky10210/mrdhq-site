9916fedb6a707d4071f11b2e3aae0b888518bd69
sky10210 <sldipasquale@gmail.com>
2026-09-01 19:50:43 -0400

    Connect Marketing Project Gallery backend

--- marketing/project-gallery/config.js
+++ marketing/project-gallery/config.js
@@ -1,1 +1,1 @@
-window.MRDHQ_PROJECT_GALLERY_API = window.MRDHQ_PROJECT_GALLERY_API || '';
+window.MRDHQ_PROJECT_GALLERY_API = 'https://script.google.com/macros/s/AKfycbxJqSiq30GSvdmNZvogZJGO32gR2o17WURwmiboKw31yO0P3P4oKXvTx7ePywyDQA/exec';
